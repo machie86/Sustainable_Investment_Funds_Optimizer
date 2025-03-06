fe204bb7ec2808b3b0eeb4b834e42b5f50ee3234
Asiyatul Mahfudloh <achiemahfudloh@gmail.com>
2025-03-07 06:14:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,11 @@ else:
             # ---- FETCH DATA FROM YFINANCE ----
             data = yf.download(tickers, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))["Close"]
             data = pd.DataFrame(data)
+            data_cleaned = data.dropna()
 
             # ---- PORTFOLIO OPTIMIZATION ----
-            mu = mean_historical_return(data)
-            S = CovarianceShrinkage(data).ledoit_wolf()
+            mu = mean_historical_return(data_cleaned)
+            S = CovarianceShrinkage(data_cleaned).ledoit_wolf()
             ef = EfficientFrontier(mu, S)
             raw_weights = ef.max_sharpe()
             cleaned_weights = ef.clean_weights()
